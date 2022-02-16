245469cc066eade5d3a9d3db9bbef02da06181ff
Pavel.Aponik <pavelaponik@gmail.com>
2022-02-16 18:45:59 +0300

    fixed packages and classes names

--- src/test/java/Onliner/pages/CataloguePage.java
+++ src/test/java/Onliner/pages/CataloguePage.java
@@ -3,50 +3,32 @@ package Onliner.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+
+import framework.Browser;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import Framework.Browser;
 
 
-public class CataloguePage {
-    private final WebDriver driver = Browser.driver;
-    private WebDriverWait wait = new WebDriverWait(Browser.driver, 10);
+public class CataloguePage extends BasePage{
     private final String catalogMenu = "//span[text()='%s']";
     private final String catalogSubMenu = "//div[contains(@class, 'catalog-navigation-list__aside-title') and contains(text(), '%s')]";
     private final String catalogDropDown = "//span[contains(@class, 'catalog-navigation-list__dropdown-title') and contains(text(), '%s')]";
 
-    public CataloguePage(WebDriver driver){
-        Browser.driver = driver;
-    }
-
-    private WebElement catMenu(String text){
-         return driver.findElement(By.xpath(String.format(catalogMenu, text)));
-    }
-
-    private WebElement catSubMenu(String text){
-        return driver.findElement(By.xpath(String.format(catalogSubMenu, text)));
-    }
-
-    private WebElement catDropDownItem(String text){
-        return driver.findElement(By.xpath(String.format(catalogDropDown, text)));
+    public CataloguePage(Browser browser){
+        super(browser);
     }
 
     public void selectCatMenu(String category){
-        wait.until(ExpectedConditions.elementToBeClickable(catMenu(category)));
-        catMenu(category).click();
+        navigateTo((String.format(catalogMenu, category))).click();
     }
 
-    public void selectCatSubMenu(String subCategory){
-        wait.until(ExpectedConditions.elementToBeClickable(catSubMenu(subCategory)));
-        Actions action = new Actions(driver);
-        action.moveToElement(catSubMenu(subCategory)).build().perform();
+    public void selectCatSubMenu(String subCategory) {
+        navigateTo(String.format(catalogSubMenu, subCategory));
     }
 
     public void selectDropDownItem(String dropDownItem){
-        wait.until(ExpectedConditions.elementToBeClickable(catDropDownItem(dropDownItem)));
-        Actions action = new Actions(driver);
-        action.moveToElement(catDropDownItem(dropDownItem)).click().build().perform();
+        browser.getActions().click(navigateTo(String.format(catalogDropDown, dropDownItem))).build().perform();
+
     }
 }
 
--- src/test/java/Onliner/pages/HomePage.java
+++ src/test/java/Onliner/pages/HomePage.java
@@ -1,33 +1,14 @@
 package Onliner.pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import Framework.Browser;
+import framework.Browser;
 
-public class HomePage {
-    private final WebDriver driver = Browser.driver;
-    private final String category = "//h2//a[contains(text(), '%s')]";
-    private JavascriptExecutor javascriptExecutor = (JavascriptExecutor) Browser.driver;
-    private WebDriverWait wait = new WebDriverWait(Browser.driver, 10);
-    Actions actionProvider = new Actions(driver);
+public class HomePage extends BasePage {
+    public static final String category = "//h2//a[contains(text(), '%s')]";
 
-    public HomePage(WebDriver driver){
-        Browser.driver = driver;
+    public HomePage(Browser browser) {
+        super(browser);
     }
 
-    public WebElement selectElement(String text){
-        return driver.findElement(By.xpath(String.format(category, text)));
-    }
 
-    public void navigateTo(String section){
-        Actions action = new Actions(driver);
-        wait.until(ExpectedConditions.visibilityOf(selectElement(section)));
-        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);", selectElement(section));
-        action.moveToElement(selectElement(section)).click().build().perform();
-    }
+
 }
