2102039f4f507dc8a91eaddd086a0c77520a52ef
Pavel.Aponik <pavelaponik@gmail.com>
2022-02-18 18:24:32 +0300

    refactor

--- src/test/java/Onliner/pages/HomePage.java
+++ src/test/java/Onliner/pages/HomePage.java
@@ -1,14 +1,13 @@
 package Onliner.pages;
 
-import framework.Browser;
+import Onliner.base.BasePage;
+import org.openqa.selenium.WebDriver;
 
 public class HomePage extends BasePage {
     public static final String category = "//h2//a[contains(text(), '%s')]";
 
-    public HomePage(Browser browser) {
-        super(browser);
+    public HomePage(WebDriver driver) {
+        super(driver);
     }
-
-
 
 }
