3b49305fabde9936f7f526a7123e3a63fb9266ec
Pavel <pavelaponik@gmail.com>
2022-03-13 14:20:19 +0300

    added changes

--- src/test/java/framework/base/BaseTest.java
+++ src/test/java/framework/base/BaseTest.java
@@ -2,14 +2,11 @@ package framework.base;
 
 
 import framework.webdriver.Browser;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 
 public class BaseTest extends Browser {
-
-    public WebDriver driver;
 
     @BeforeClass
     public void before(){
--- src/test/java/onliner/tests/TestRunner.java
+++ src/test/java/onliner/tests/TestRunner.java
@@ -3,9 +3,9 @@ package onliner.tests;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = {"src/test/java/onliner/features/OnlinerTest.feature"},
+@CucumberOptions(features = {"src/test/resources/OnlinerTest.feature"},
         glue = "onliner/stepsdefenition",
-        plugin = {"pretty", "html:target/cucumber.html",
+        plugin = {"pretty", "html:target/cucumber-html",
                 "json:target/cucumber.json","json:target/cucumber-reports/CucumberTestReport.json"})
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
