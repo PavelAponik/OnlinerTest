96c4e26143f1935c1d0d1fc4dbb071fedac630f3
Pavel.Aponik <pavelaponik@gmail.com>
2022-03-24 14:26:56 +0300

    added changes

--- src/test/java/onliner/pageObjects/TVPage.java
+++ src/test/java/onliner/pageObjects/TVPage.java
@@ -9,22 +9,20 @@ import org.openqa.selenium.By;
 public class TVPage extends BasePage {
 
     public static final String CHECKBOX_LOCATOR = "//li/label[contains(@class, 'schema-filter__checkbox-item')]/span[contains(text(), '%s')]";
-    public static final String MAX_PRICE_LOCATOR = "//input[contains(@class, 'schema-filter-control__item schema-filter__number-input schema-filter__number-input_price')][@placeholder='до']";
-    public static final String PRODUCT_TITLE = "//div[@class='schema-product__title']";
+    public final TextBox txbMaxPrice = new TextBox(By.xpath("//input[contains(@class, 'schema-filter-control__item schema-filter__number-input schema-filter__number-input_price')][@placeholder='до']"),"Max Price");
 
     public TVPage(){
         super(By.xpath("//div[contains(@class, 'b-top-logo')]"), "TV Page");
     }
 
     public void selectCheckbox(String checkboxName){
-        Checkbox checkbox = new Checkbox(By.xpath(String.format(CHECKBOX_LOCATOR, checkboxName)), checkboxName);
-        checkbox.scrollToElement();
-        checkbox.clickAndWait();
+        Checkbox chbCheckbox = new Checkbox(By.xpath(String.format(CHECKBOX_LOCATOR, checkboxName)), checkboxName);
+        chbCheckbox.scrollToElement();
+        chbCheckbox.clickAndWait();
     }
 
     public void setPrice(String price){
-        TextBox txtPrice = new TextBox(By.xpath(String.format(MAX_PRICE_LOCATOR, price)), price);
-        txtPrice.scrollToElement();
-        txtPrice.sendValue(price);
+        txbMaxPrice.scrollToElement();
+        txbMaxPrice.sendValue(price);
     }
 }
